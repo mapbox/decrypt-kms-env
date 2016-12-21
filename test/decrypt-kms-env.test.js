43256c199399efca3f098b30c78edabd9fe35d8f
Young Hahn <yfhahn@gmail.com>
2016-12-21 14:53:59 -0500

    Add tests for JS api

--- test/decrypt-kms-env.test.js
+++ test/decrypt-kms-env.test.js
@@ -40,3 +40,52 @@ tape('decrypt-kms-env: secure env vars', function(assert) {
   });
 });
 
+
+tape('js dke: errors without region', function(assert) {
+  dke({}, function(err, output) {
+    assert.deepEqual(err && err.toString(), 'Error: AWS_DEFAULT_REGION env var must be set', 'errors');
+    assert.end();
+  });
+});
+
+tape('js dke: no secure env vars', function(assert) {
+  dke({
+    AWS_DEFAULT_REGION: 'us-east-1'
+  }, function(err, output) {
+    assert.ifError(err, 'no error');
+    assert.deepEqual(output, {}, 'no output');
+    assert.end();
+  });
+});
+
+tape('js dke', function(assert) {
+  AWS.mock('KMS', 'decrypt', function(params, callback) {
+    var encrypted = new Buffer(params.CiphertextBlob, 'base64').toString('utf8');
+    if (encrypted === 'EncryptedValue1')
+      return callback(null, { Plaintext: (new Buffer('DecryptedValue1')).toString('base64') });
+    if (encrypted === 'EncryptedValue2')
+      return callback(null, { Plaintext: (new Buffer('DecryptedValue2')).toString('base64') });
+    assert.fail('Unrecognized encrypted value ' + encrypted);
+  });
+  var env = {
+    AWS_DEFAULT_REGION: 'us-east-1',
+    SecureVarA: 'secure:' + (new Buffer('EncryptedValue1')).toString('base64'),
+    SecureVarB: 'secure:' + (new Buffer('EncryptedValue2')).toString('base64'),
+    NormalVarC: 'Hello World'
+  };
+  dke(env, function(err, output) {
+    assert.ifError(err, 'no error');
+    assert.deepEqual(env, {
+      AWS_DEFAULT_REGION: 'us-east-1',
+      SecureVarA: 'DecryptedValue1',
+      SecureVarB: 'DecryptedValue2',
+      NormalVarC: 'Hello World'
+    }, 'Sets decrypted values');
+    assert.deepEqual(output, {
+      SecureVarA: '************lue1',
+      SecureVarB: '************lue2',
+    }, 'Debug output');
+    assert.end();
+  });
+});
+
